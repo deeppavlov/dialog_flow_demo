b3b46249c1407ee85dd5c78a88789c883b2efbb2
ruthenian8 <ruthenian8@gmail.com>
2023-02-21 19:48:57 +0300

    Update dialog_flow_chatgpt_skill/dff_chatgpt_skill/response.py
    Co-authored-by: Roman Zlobin <trlkrot@gmail.com>

--- dialog_flow_chatgpt_skill/dff_chatgpt_skill/response.py
+++ dialog_flow_chatgpt_skill/dff_chatgpt_skill/response.py
@@ -3,6 +3,7 @@ from dff.script import Context, Actor, Message
 from . import utils
 
 
+
 def choose_response(ctx: Context, actor: Actor) -> Message:
     """
     Return ChatGPT response if it is coherent, fall back to
