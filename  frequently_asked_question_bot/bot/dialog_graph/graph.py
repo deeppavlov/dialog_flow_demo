33f239b2ddcd2a9dfb3fbc8009e90e0cf8a589c8
Denis Kuznetsov <kuznetsov.den.p@gmail.com>
2023-02-25 00:10:35 +0300

    refactor/faq_bot: refactor faq bot

---  frequently_asked_question_bot/bot/dialog_graph/graph.py
+++  frequently_asked_question_bot/bot/dialog_graph/graph.py
@@ -1,5 +1,5 @@
 """
-Script
+Graph
 ------
 This module defines a script that the bot follows during conversation.
 """
@@ -10,29 +10,25 @@ from dff.messengers.telegram import TelegramMessage
 from .responses import answer_question, suggest_similar_questions
 from .conditions import received_button_click, received_text
 
-qa_transitions = {
-    ("qa_flow", "suggest_questions"): received_text,
-    ("qa_flow", "answer_question"): received_button_click,
-}
-
 script = {
-    "proxy_flow": {
+    "service_flow": {
         "start_node": {
-            RESPONSE: TelegramMessage(),
-            TRANSITIONS: {"welcome_node": cnd.exact_match(TelegramMessage(text="/start"))},
-        },
-        "welcome_node": {
-            RESPONSE: TelegramMessage(text="Welcome! Ask me questions about Arch Linux."),
-            TRANSITIONS: qa_transitions,
+            TRANSITIONS: {("qa_flow", "welcome_node"): cnd.exact_match(TelegramMessage(text="/start"))},
         },
         "fallback_node": {
             RESPONSE: TelegramMessage(text="Something went wrong. Use `/restart` to start over."),
-            TRANSITIONS: {"welcome_node": cnd.exact_match(TelegramMessage(text="/restart"))},
+            TRANSITIONS: {("qa_flow", "welcome_node"): cnd.exact_match(TelegramMessage(text="/restart"))},
         },
     },
     "qa_flow": {
         LOCAL: {
-            TRANSITIONS: qa_transitions,
+            TRANSITIONS: {
+                ("qa_flow", "suggest_questions"): received_text,
+                ("qa_flow", "answer_question"): received_button_click,
+            },
+        },
+        "welcome_node": {
+            RESPONSE: TelegramMessage(text="Welcome! Ask me questions about Arch Linux."),
         },
         "suggest_questions": {
             RESPONSE: suggest_similar_questions,
--- faq_bot/run.py
+++
@@ -1,7 +0,0 @@
-from bot.bot import pipeline
-
-if __name__ == "__main__":
-    if pipeline.messenger_interface.messenger.token == "":
-        raise RuntimeError("Token is not set.")
-
-    pipeline.run()
