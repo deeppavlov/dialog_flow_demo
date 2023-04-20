92155c8d132177e1f57a6611f5619d706b5514c3
ruthenian8 <ruthenian8@gmail.com>
2023-04-20 16:54:29 +0300

    debug dnnc intent retrieval function

--- customer_service_bot/bot/api/dnnc.py
+++ customer_service_bot/bot/api/dnnc.py
@@ -4,17 +4,20 @@ DNNC
 This module includes queries to a local DNNC service.
 """
 import requests
+from dff.script import Message
 
 
 DNNC_SERVICE = "http://localhost:4999/respond"
 
 
-def get_intents(request: str):
+def get_intents(request: Message):
     """
     Query the local DNNC service extracting intents from the
     last user utterance.
     """
-    request_body = {"dialog_contexts": [request]}
+    if not request.text:
+        return []
+    request_body = {"dialog_contexts": [request.text]}
     try:
         response = requests.post(DNNC_SERVICE, json=request_body)
     except requests.RequestException:
